3e16a77b1403aa9106a76b5fe2ccff558348d239
Nguyen Cong Duy <ncongduy@gmail.com>
2021-07-13 01:11:10 +0300

    update: add feature 'who serve?'

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
+  const [team1Member1, setTeam1Member1] = useState("");
+  const [team1Member2, setTeam1Member2] = useState("");
+  const [team2Member1, setTeam2Member1] = useState("");
+  const [team2Member2, setTeam2Member2] = useState("");
+
   const reset = () => {
     setHomeScore(0);
     setAwayScore(0);
+    setTeam1Member1("");
+    setTeam1Member2("");
+    setTeam2Member1("");
+    setTeam2Member2("");
   };
 
   return (
@@ -15,8 +24,48 @@ function App() {
       <table>
         <thead>
           <tr>
-            <th className="home">Tiimi 1</th>
-            <th className="away">Tiimi 2</th>
+            <th>
+              <label>
+                <input
+                  type="text"
+                  className="team1"
+                  value={team1Member1}
+                  onChange={(e) => setTeam1Member1(e.target.value)}
+                ></input>
+              </label>
+              <input name="team1" type="radio" value="member1"></input>
+
+              <label>
+                <input
+                  type="text"
+                  className="team1"
+                  value={team1Member2}
+                  onChange={(e) => setTeam1Member2(e.target.value)}
+                ></input>
+              </label>
+              <input name="team1" type="radio" value="member2"></input>
+            </th>
+            <th>
+              <label>
+                <input
+                  type="text"
+                  className="team2"
+                  value={team2Member1}
+                  onChange={(e) => setTeam2Member1(e.target.value)}
+                ></input>
+              </label>
+              <input name="team2" type="radio" value="member1"></input>
+
+              <label>
+                <input
+                  type="text"
+                  className="team2"
+                  value={team2Member2}
+                  onChange={(e) => setTeam2Member2(e.target.value)}
+                ></input>
+              </label>
+              <input name="team2" type="radio" value="member2"></input>
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -37,7 +86,7 @@ function App() {
         </tfoot>
       </table>
 
-      <button onClick={reset}>Tyhjätä</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
